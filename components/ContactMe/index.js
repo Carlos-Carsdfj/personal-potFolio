d8cfc593f68ece6a5716d264e83948b4da29f425
carsdf <carsdfj@gmail.com>
2021-05-23 21:56:19 -0400

    changing the contact design

--- components/ContactMe/index.js
+++ components/ContactMe/index.js
@@ -1,11 +1,23 @@
 import ContactForm from './ContactForm'
 import styles from './ContactMe.module.css'
 import { motion } from 'framer-motion'
+import SvgComponent from './SvgTablet'
+import { useState } from 'react'
 
 
 export default function ContactMe(){
+ 
+    const [isOn, setIsOn] = useState(false)
 
-
+    const variantContact={
+        hidden:{scale:0, x:'-400px'},
+        visible:{scale:1,x:'0'},
+        transition:{
+            type: "spring",
+            stiffness: 260,
+            damping: 50
+        }
+    }
     const variantContent={
         hidden:{scale:0},
         visible:{scale:1},
@@ -16,12 +28,23 @@ export default function ContactMe(){
         }
     }
 
+    const handlerClick = () =>{
+
+
+        setIsOn(prev => !prev)
+    }
+
     return(<motion.div className={styles.content}
      variants={variantContent}
             initial='hidden'
             animate='visible'
             transition='transition' >
-            <ContactForm></ContactForm>
+            <SvgComponent btn={handlerClick} isOn={isOn}></SvgComponent>
+            {isOn ? <motion.div className={styles.contactContent} variants={variantContact}
+            initial='hidden'
+            animate='visible'
+            transition='transition'><ContactForm ></ContactForm> </motion.div>  : '' }
+            
             
     </motion.div>)
 }
